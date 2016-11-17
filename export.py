e78160d08d885127a8fefb2f03c0a376ddb91256
Sziszka0909 <szaboszilveszter0909@gmail.com>
2016-11-17 13:32:39 +0100

    Update export.py

--- export.py
+++ export.py
@@ -1,2 +1,22 @@
 
 # Export functions
+
+
+import reports
+
+file_name = 'game_stat.txt'
+title = 'The Sims 3'
+total_copies_sold = '10'
+year = '2010'
+genre = 'Simulation'
+publisher = 'Microsoft'
+
+line0 = "Number of games: %s" % (reports.count_games(file_name))
+line1 = "Is there a game from %s?: %s" %(year, reports.decide(file_name, year))
+line2 = "%s is the latest game on the list." % (reports.get_latest(file_name))
+line3 = "There is %s game with the genre %s" % (reports.count_by_genre(file_name, genre), genre)
+line4 = "The %s game is in the %d-th line." % (title, reports.get_line_number_by_title(file_name, title))
+text_list = [line1, line2, line3, line4]
+with open("Judy's answers.txt", "w") as answertext:
+    for k in range(len(text_list)):
+        answertext.writelines(text_list[k]+"\n")
